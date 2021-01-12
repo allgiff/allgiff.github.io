b3e746773ba4386ca60853c5bce5f59454eafc54
allgiff <gif19002@byui.edu>
2021-01-12 08:27:13 -0700

    test commit

--- Javascript/index.js
+++ Javascript/index.js
@@ -6,13 +6,4 @@ window.addEventListener('load', (event)=> {
     cry.textContent = new Date().getFullYear();
 })
 
-//let dateNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-//let d = new Date(); 
-//let dayName = dateNames[d.getDay()];
-//let monthName = months[d.getMonth()];
-//let year = d.getFullYear(); 
-//let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
-
-//document.getElementById("currentdate").textContent = fulldate; 
