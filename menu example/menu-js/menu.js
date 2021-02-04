011d70645bec6a05b3dbda70da72e44f887038f8
allgiff <gif19002@byui.edu>
2021-02-04 08:38:59 -0700

    created lesson 5 file

--- menu example/menu-js/menu.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    const hambutton = document.querySelector('.ham');
-    const mainnav = document.querySelector('#navigation');
-
-    hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
-
-    window.onresize = () => { if (window.innerwidth > 760) mainnav.classList.remove('responsive'); };
-});
