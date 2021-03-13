906e0312f17b799a43ef41860d2040e76938dc76
allgiff <gif19002@byui.edu>
2021-03-13 13:07:56 -0700

    bug fixes

--- lesson 10/preston 10/js/preston-6.js
+++ lesson 10/preston 10/js/preston-6.js
@@ -75,12 +75,3 @@ fetch(apiUrl)
         document.getElementById('humidity').innerHTML = table.main.humidity;
         document.getElementById('speed').innerHTML = table.wind.speed;
     })
-
-let day = new Array(7);
-weekday[0] = "Monday";
-weekday[1] = "Tuesday";
-weekday[2] = "Wednesday";
-weekday[3] = "Thursday";
-weekday[4] = "Friday";
-weekday[5] = "Saturday";
-weekday[6] = "Sunday";
