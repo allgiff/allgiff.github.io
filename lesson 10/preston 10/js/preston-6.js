0bdf8d2911ef97f197d847d15448ecc6a4477150
allgiff <gif19002@byui.edu>
2021-03-13 13:03:41 -0700

    updated links

--- lesson 10/preston 10/js/preston-6.js
+++ lesson 10/preston 10/js/preston-6.js
@@ -60,8 +60,8 @@ if (day == 4) {
     document.getElementById('pancake').style.display = "none";
 }
 //javascript for the weather summary and 5 day forcasts//
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather?id=5605242&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial"
-const apiUrl2 = "http://api.openweathermap.org/data/2.5/weather?id=5605242&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial"
+const apiUrl2 = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=532c1b5f6ed1af0e5f24414cb15bc219&units=imperial"
 
 fetch(apiUrl)
     .then(Response => { return Response.json();})
@@ -76,3 +76,11 @@ fetch(apiUrl)
         document.getElementById('speed').innerHTML = table.wind.speed;
     })
 
+let day = new Array(7);
+weekday[0] = "Monday";
+weekday[1] = "Tuesday";
+weekday[2] = "Wednesday";
+weekday[3] = "Thursday";
+weekday[4] = "Friday";
+weekday[5] = "Saturday";
+weekday[6] = "Sunday";
