b628b046de513f05532a617c9dfd16c4762f1341
allgiff <gif19002@byui.edu>
2021-04-03 11:45:14 -0600

    sdfghjk

--- lesson 13/home/index.js
+++ lesson 13/home/index.js
@@ -6,13 +6,3 @@ window.addEventListener('load', () => {
 
     window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive'); };
 });
-
-//javascript for nav - hamburger menu
-window.addEventListener('load', () => {
-    const hambutton = document.querySelector('.ham');
-    const mainnav = document.querySelector('#navigation');
-
-    hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
-
-    window.onresize = () => { if (window.innerwidth > 760) mainnav.classList.remove('responsive'); };
-});
