ea0e0856c0f7f5346c26e1721a0650dc41173f6c
allgiff <gif19002@byui.edu>
2021-09-28 13:03:40 -0600

    added notes page to javascript

--- Fall 2021/WDD 330/js/main.js
+++ Fall 2021/WDD 330/js/main.js
@@ -7,7 +7,7 @@ const options = [
         url: "https://allgiff.github.io/Fall%202021/WDD%20330/Week%202/week2notes.html"
     }, {
         label: "Week 3 notes",
-        url: ""
+        url: "https://allgiff.github.io/Fall%202021/WDD%20330/Week%203/week3notes.html"
     }
 ]
  
