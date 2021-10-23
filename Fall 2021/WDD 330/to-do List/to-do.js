f29abfedb27a603afc48d2c5c9ce886be2d26f76
allgiff <gif19002@byui.edu>
2021-10-22 20:36:17 -0600

    help me please

--- Fall 2021/WDD 330/to-do List/to-do.js
+++ Fall 2021/WDD 330/to-do List/to-do.js
@@ -77,9 +77,9 @@ function filterItems() {
       document.getElementById(id).classList.add("selected");
       document.querySelector(".checked").className = `checked ${id}`;
       if (checked === true) {
-        checked.style.display = none;
+        checked.style.visibility = hidden;
       }
-      
+
     }
   });
 }
