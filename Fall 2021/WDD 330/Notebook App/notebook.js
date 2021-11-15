fb365af8950b408855d5c81051abc7d094133397
allgiff <gif19002@byui.edu>
2021-11-15 14:52:19 -0700

    test

--- Fall 2021/WDD 330/Notebook App/notebook.js
+++ Fall 2021/WDD 330/Notebook App/notebook.js
@@ -1,8 +1,26 @@
-import NotesAPI from "./NotesAPI";
+function createNote() {
+    
+}
 
-NotesAPI.saveNote({
-    title: "New Note",
-    body: "I am a new note"
-});
+function getAllNotes() {
+    const notes = JSON.parse(localStorage.getItem("notesApp-notes") || "[]");
 
-console.log(NotesAPI.getAllNotes());
+    return notes.sort((a, b) => {
+        return new Date(a.updated) > Date(b.updated) ? -1 : 1;
+    });
+}
+
+function saveNote(noteToSave) {
+    const notes = getAllNotes();
+
+    noteToSave.id = Math.floor(Math.random() * 1000000);
+    noteToSave.updated = new Date().toISOString();
+    notes.push(noteToSave);
+
+    localStorage.setItem("notesApp-notes", JSON.stringify(notes));
+}
+
+function deleteNote(id) {
+    
+}
+
