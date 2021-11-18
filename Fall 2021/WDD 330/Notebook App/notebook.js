bf4464d5ac6d50861b9c59a915e4b6bbcad21aba
allgiff <gif19002@byui.edu>
2021-11-18 15:24:39 -0700

    AHHHH

--- Fall 2021/WDD 330/Notebook App/notebook.js
+++ Fall 2021/WDD 330/Notebook App/notebook.js
@@ -1,4 +1,50 @@
 function createNote() {
-    let target = document.getElementsByClassName("notesList");
+    let grabThis = document.querySelector('.notesList');
+    let grabMainNote = document.querySelector('.notesPreview');
+
+    //This is for the sidebar, under the add notes button.
+    //create and add a class for the whole list item
+    var notesListItem = document.createElement('div');
+    notesListItem.classList.add('notesListItem');
+    grabThis.appendChild(notesListItem);
+
+    //create and add class for the title
+    var noteSmallTitle = document.createElement('div');
+    noteSmallTitle.classList.add('noteSmallTitle');
+    noteSmallTitle.innerHTML += 'Note Title';
+    grabThis.appendChild(noteSmallTitle);
+
+    //create and add class for the body
+    var noteSmallBody = document.createElement('div');
+    noteSmallBody.classList.add('noteSmallBody');
+    noteSmallBody.innerHTML += 'Note Content';
+    grabThis.appendChild(noteSmallBody);
+
+    //create and add class for last updated date
+    var noteSmallUpdated = document.createElement('div');
+    noteSmallUpdated.classList.add('noteSmallUpdated');
+    noteSmallUpdated.innerHTML += 'Last Updated:';
+    grabThis.appendChild(noteSmallUpdated);
+
+    //this is for the main note that you write in
+    //create the title of the note and assign a class
+    var notesTitle = document.createElement('input');
+    notesTitle.classList.add('notesTitle');
+    grabMainNote.appendChild(notesTitle);
+    notesTitle.placeholder = 'Note Title';
+
+    //create the body of the note and assign a class and placeholder
+    var noteBody = document.createElement('textarea');
+    noteBody.classList.add('noteBody');
+    grabMainNote.appendChild(noteBody);
+    noteBody.placeholder = 'Start Writing!';
+}
+
+function userContent() {
+    // take the note title that the user adds into the input
+    // add the user input into the note title in the side bar
+}
+
+function saveNote() {
 
 }
