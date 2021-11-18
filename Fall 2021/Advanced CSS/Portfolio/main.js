84c9b0b4f06add63d577419d3d0ce2336a15bf8a
allgiff <gif19002@byui.edu>
2021-11-18 16:35:20 -0700

    AHHHHHH

---
+++ Fall 2021/Advanced CSS/Portfolio/main.js
@@ -0,0 +1,26 @@
+const options = [
+    {
+        label: "__ Assignment",
+        url: "#"
+    }, {
+        label: "BYUI Remake Assignment",
+        url: "#"
+    }
+
+]
+ 
+function makeUL(options) {
+    let list = document.getElementById('myList');
+
+    for (let i = 0; i < options.length; i++) {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.href = options[i].url;
+        var link = document.createTextNode(options[i].label);
+        a.appendChild(link);
+        li.appendChild(a);
+        list.appendChild(li);
+    }
+    return list;
+}
+makeUL(options);
