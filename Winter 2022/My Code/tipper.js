0830a440da8e83cfbf509ef90e0f22c4968860e7
allgiff <gif19002@byui.edu>
2022-02-27 00:27:08 -0700

    I did it!

--- Winter 2022/My Code/tipper.js
+++ Winter 2022/My Code/tipper.js
@@ -1,4 +1,4 @@
-function tipCalc() {
+/*function tipCalc() {
     let billAmount = document.getElementById("cost-input").value;
     let serviceQual = document.getElementById("service").value;
 
@@ -17,5 +17,28 @@ function tipCalc() {
     
     //show the user the tip
     let display = document.querySelector("#tip");
-    display.innerHTML = total;
+    display += innerHTML = total;
+}; */
+
+//Calculate Tip
+
+function tipCalc() {
+  var billAmt = document.getElementById("cost-input").value;
+  var serviceQual = document.getElementById("service").value;
+
+  //validate input
+  if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
+  }
+
+  //Calculate tip
+  var total = (billAmt * serviceQual);
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+  //Display the tip
+  document.getElementById("tip").innerHTML = total;
+
 };
